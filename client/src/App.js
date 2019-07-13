99c9af6fe7f43681bd2639bf6b2eeda2ffac4ea3
roy bm <roiybm@gmail.com>
2019-07-13 12:04:22 +0300

    next 1.4

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/layouts/Home';
+import About from './components/layouts/About';
+import Layout from './components/layouts/Layout';
+import NewsItemDetail from './components/presentation/NewsItemDetail';
+
+ 
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path='/news/:id' component={NewsItemDetail}/>    
+            </Layout>
+        </BrowserRouter>
+    );
+  }
+}
+
+export default App;
